e2bb5be8e12f98452e71ad5d576523de6cd5cca0
Jared Baur <jaredbaur@fastmail.com>
2023-03-03 14:50:45 -0800

    Provide a simple web server for serving wireguard configs

---
+++ wg-config-server/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseConfig(t *testing.T) {
+	tt := []struct {
+		config  string
+		parsed  string
+		privKey string
+		err     error
+	}{
+		{
+			config:  "",
+			parsed:  "",
+			privKey: "",
+			err:     errInvalidConfig,
+		},
+		{
+			config:  "[Interface]\nPrivateKey=foobar\n",
+			parsed:  "[Interface]\nPrivateKey=foobar\n",
+			privKey: "foobar",
+			err:     nil,
+		},
+	}
+
+	for _, tc := range tt {
+		parsed, privKey, err := parseConfig(tc.config)
+		if tc.err != err {
+			t.Fatalf("wanted error: %v, got error: %v\n", tc.err, err)
+		}
+		if tc.parsed != parsed {
+			t.Fatalf("wanted parsed:\n%v, got parsed:\n%v\n", tc.parsed, parsed)
+		}
+		if tc.privKey != privKey {
+			t.Fatalf("wanted privKey: %v, got privKey: %v\n", tc.privKey, privKey)
+		}
+	}
+}
