b49f9703e4bc0a00ab60631eb277eff5d6ae9ee7
Jared Baur <jaredbaur@fastmail.com>
2023-03-13 13:39:21 -0700

    Use builtin firewall module

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # NixOS Router
 
 A NixOS module for configuring a super simple router/firewall device.
-
-## Requirements
-
-- You must generate a unique local (ULA) /48 IPv6 network prefix
-- You must use nftables for extra firewall configuration. The firewall built by
-  this module uses nftables, so loading the iptables kernel modules will
-  conflict with the nftables kernel module.
--- test.nix
+++ test.nix
@@ -1,18 +1,37 @@
 { nixosTest, module, ... }:
+let
+  host1Mac = "70:ca:6e:62:ab:f6";
+in
 nixosTest {
   name = "router";
   nodes.router = { ... }: {
     imports = [ module ];
+    virtualisation.vlans = [ 1 ];
+    systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
     router = {
       enable = true;
-      ipv4Prefix = "192.168.1.0/24";
-      ipv6UlaPrefix = "fc00::/64";
-      wanInterface = "wan";
-      wanSupportsDHCPv6 = true;
-      heTunnelBroker.enable = false;
-      lanInterface = "lan";
-      hosts.h1 = { id = 2; mac = "70:ca:6e:62:ab:f6"; };
+      ipv4Prefix = "192.168.0.0/24";
+      ipv6UlaPrefix = "fdc8:2291:4584:0::/64";
+      wanInterface = "eth0";
+      lanInterface = "eth1";
+      hosts.host1 = { id = 2; mac = host1Mac; };
     };
   };
+
+  nodes.host1 = { config, pkgs, ... }: {
+    virtualisation.vlans = [ 1 ];
+    systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
+    systemd.network.networks."40-eth1".dhcpV4Config.ClientIdentifier = "mac";
+    networking = {
+      useNetworkd = true;
+      useDHCP = false;
+      firewall.enable = false;
+      interfaces.eth1 = {
+        useDHCP = true;
+        macAddress = host1Mac;
+      };
+    };
+  };
+
   testScript = builtins.readFile ./test.py;
 }
