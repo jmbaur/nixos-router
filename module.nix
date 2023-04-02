bcf0760105492d93a006313b4d64c031895e7bc4
Jared Baur <jaredbaur@fastmail.com>
2023-04-02 09:36:02 -0700

    Fix hostapd startup order

--- module.nix
+++ module.nix
@@ -8,15 +8,21 @@
     ./wan.nix
   ];
 
-  config = lib.mkIf config.router.enable {
-    services.avahi = {
-      enable = false;
-      openFirewall = false;
-    };
+  config = lib.mkIf config.router.enable (lib.mkMerge [
+    {
+      services.avahi = {
+        enable = false;
+        openFirewall = false;
+      };
 
-    services.openntpd.enable = true;
+      services.openntpd.enable = true;
 
-    networking.useDHCP = lib.mkForce false;
-    systemd.network.enable = true;
-  };
+      networking.useDHCP = lib.mkForce false;
+      systemd.network.enable = true;
+    }
+    (lib.mkIf config.services.hostapd.enable {
+      # fix hostapd startup ordering
+      systemd.services.hostapd.before = [ "systemd-networkd.service" ];
+    })
+  ]);
 }
